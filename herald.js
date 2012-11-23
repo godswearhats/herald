67a024165036368837d6bfa575c9d52079953326
Aidan <github@godswearhats.com>
2012-11-23 15:38:08 -0800

    Added scenery

--- herald.js
+++ herald.js
@@ -2,21 +2,107 @@ console.log("Loaded herald.js");
 var unitBeingRotated = false;
 var mouseStartAngle = false;
 var unitStartAngle = false;
-var battleUnits = { '#player-1': [], '#player-2': [] };
+var battleUnits = { '#player-1': [], '#player-2': [], '#scenery': [] };
+var gameStarted = false;
 
 $(function() {
 	$(document).on('mouseup', stopRotate);
 	$( ".add-unit" ).on("click", addUnit);
+	initializeSceneryManager();
+	$( '#control-panel' ).tabs({
+		activate: function(event, ui) {
+			$('.unit').draggable( "disable" );
+			var player = ui.newPanel.data('player');
+			if (player) {
+				$('.unit').draggable( "disable" );
+				$('.unit.player-' + player).draggable( "enable" );
+			}
+			else {
+				$('.unit').draggable( "enable" );
+			}
+		}
+	});
 	$( "#dump-button" ).on("click", dumpHTML);
 	$( "#slurp-button" ).on("click", slurpHTML);
+	$( "#start-game").on("click", toggleGameState);
 });
 
+function toggleGameState(event) {
+	if (gameStarted) {
+		gameStarted = false;
+		$("#start-game").text("Start Game (locks scenery)");
+		$('.scenery').draggable( {cancel :'.handle'} ).resizable({
+			autoHide: true,
+			aspectRatio: true
+		});
+	}
+	else {
+		gameStarted = true;
+		$("#start-game").text("Stop Game (unlocks scenery)");
+		$('.scenery').draggable( "destroy" ).resizable( "destroy" );
+	}
+}
+
+function initializeSceneryManager() {
+	var sm = $('#scenery-manager');
+	var scenery = ['hill', 'corner_hill', 'tree', 'tower', 'wall'];
+	for (var i = 0; i < scenery.length; i++) {
+		var image_src = 'img/' + scenery[i] + '.png';
+		$( document.createElement('img') )
+		    .attr({
+				src: image_src,
+				alt: scenery[i].replace("_", " ")
+			})
+		    .load(function() {
+				$( document.createElement('button') )
+					.appendTo(sm)
+					.on('click', { src: this.src, height: this.height, width: this.width }, addScenery)
+					.text( this.alt );
+		    });
+	}
+}
+
+function addScenery(event) {
+	var sceneryID = "scenery-" + battleUnits['#scenery'].length;
+	battleUnits['#scenery'].push(sceneryID);
+	var scenery = $( document.createElement('div') )
+		.addClass('scenery')
+		.attr('id', sceneryID)
+		.data('angle', 0)
+		.draggable( {cancel: '.handle'} )
+		.css({
+			'position': 'absolute',
+			'height': event.data.height + "px",
+			'width': event.data.width + "px",
+			'z-index': 10,
+			'background-image': 'url(' + event.data.src + ')',
+			'background-repeat': 'none'
+		})
+		.resizable({
+			autoHide: true,
+			aspectRatio: true
+		})
+		.hover( 
+			function() {
+				if (!gameStarted) {
+					$(this).children('.control').show();
+				}
+			},
+			function() { 
+				if (!gameStarted) {
+					$(this).children('.control').hide(); 
+				}
+			}
+		);
+	$('#battlefield').append(scenery);
+	addHandle(scenery);
+	addDeleteButton(scenery);
+}
 
 function addUnit(event) {
-	var playerNumber = event.target.parentElement.dataset.player;
+	var playerNumber = event.target.parentElement.parentElement.dataset.player;
 	var playerClass = "#player-" + playerNumber;
 	var unitID = "player-" + playerNumber + "-unit-" + battleUnits[playerClass].length;
-	console.log("Adding unit " + unitID + " for " + playerClass);
 	battleUnits[playerClass].push(unitID);
 	
 	// create unit
@@ -48,18 +134,20 @@ function addUnit(event) {
 }
 
 function removeFromBattle(unitID) {
-	var playerClass = "#" + unitID.match(/player-\d/)[0];
-	battleUnits[playerClass].splice( battleUnits[playerClass].indexOf(unitID) );
+	console.log("attempting to remove " + unitID);
+	var unitClass = "#" + unitID.match(/player-\d|scenery/)[0];
+	battleUnits[unitClass].splice( battleUnits[unitClass].indexOf(unitID) );
 }
 
 function addDeleteButton(unit) {
 	var $confirmDialog = $( document.createElement('div') )
 		.data( 'unit', unit.attr('id') )
-		.attr('title', "Delete this unit?")
+		.attr('title', "Delete this?")
 		.addClass('dialog')
 		.dialog({
 	            resizable: false,
-	            height: 'auto',
+	            height: '40px',
+				width: '160px',
 	            modal: true,
 				autoOpen: false,
 	            buttons: {
@@ -85,14 +173,11 @@ function addDeleteButton(unit) {
 		    'right': 5,
 		    'height': 10,
 		    'width': 10,
-			'color': 'black',
-			'font-size': 9,
-			'font': 'sans-serif',
 			'cursor': 'pointer',
-		    'background-color': '#CCC'
+			'background-image': 'url(img/delete.png)',
+			'background-size': '100%'
 		})
 		.hide()
-		.text('X')
 		.on('click', function() {
 			console.log("Attempting to open dialog");
 			$confirmDialog.dialog("open");
@@ -111,11 +196,12 @@ function addHandle(unit) {
 		.css({
 			'position': 'absolute',
 			'bottom': 5,
-			'right': 5,
+			'left': 5,
 			'height': 10,
 			'width': 10,
 			'cursor': 'pointer',
-			'background-color': 'orange'
+			'background-image': 'url(img/rotate.png)',
+			'background-size': '100%'
 		})
 		.draggable({
 		    handle: '#handle',
@@ -145,14 +231,12 @@ function rotateUnit(event) {
 	unitBeingRotated.css('-webkit-transform','rotate(' + rotateAngle + 'rad)');
 	unitBeingRotated.css('-o-transform','rotate(' + rotateAngle + 'rad)');
 	unitBeingRotated.data('angle', rotateAngle );
-	console.log("DATA ANGLE: " + unitBeingRotated.data('angle'));
 	
 	return false;
 }
 
 function startRotate(event) {
 	// if (!event.shiftKey) return;
-	console.log("Start rotation for " + $(this).parent());
 	unitBeingRotated = $(this).parent();
 	
 	var center = getUnitCenter( unitBeingRotated );
@@ -168,7 +252,6 @@ function startRotate(event) {
 
 function stopRotate( event ) {
 	if ( !unitBeingRotated ) return;
-	console.log("Stopping rotation");
 	$(document).unbind( 'mousemove' );
 	setTimeout( function() { unitBeingRotated = false; }, 10 );
 	return false;
@@ -180,7 +263,6 @@ function dragStart( event, ui ) {
 
 // taken from http://www.elated.com/articles/smooth-rotatable-images-css3-jquery/
 function getUnitCenter(unit) {
-	console.log("Getting center for " + unit);
 	 // Rotate the image to 0 radians
 	 unit.css('transform','rotate(0rad)');
 	 unit.css('-moz-transform','rotate(0rad)');
