c1818c361874a7c3ec473d1df87cf46f5a9daaa4
Aidan Rogers <arogers@snapchat.com>
2018-03-07 15:56:21 -0800

    UI and code cleanup

--- www/js/shared.js
+++ www/js/shared.js
@@ -27,6 +27,69 @@ function getNerve(waver, rout) {
   return waver + '/' + rout
 }
 
+function makeArtifactEntry(name, entry) {
+  var item = document.createElement('li')
+  var anchor = document.createElement('a')
+  $(anchor).text(name)
+  $(anchor).attr('href', '#build')
+  var points = document.createElement('span')
+  $(points).addClass('ui-li-count')
+  $(points).text(entry.points)
+  anchor.appendChild(points)
+  item.appendChild(anchor)
+  return item
+  // TODO add description and rules
+}
+
+// Creates an entry for a unit, with all it's size choices.
+function makeUnitEntry(name, entry) {
+  var item = document.createElement('div')
+  var type = (entry.type >= 10)
+    ?	'Hero (' + types[entry.type - 10] + ')'
+    :	types[entry.type]
+
+  // expand if more than one unit choice (e.g. Troop, Regiment, Horde)
+  var header = document.createElement('h2')
+  header.innerHTML = name
+  item.appendChild(header)
+  item.setAttribute('data-role', 'collapsible')
+  item.setAttribute('data-filtertext', name + ' ' + type)
+  item.setAttribute('data-iconpos', 'right')
+  item.setAttribute('data-collapsed-icon', 'carat-d')
+  item.setAttribute('data-expanded-icon', 'carat-u')
+  item.setAttribute('data-inset', 'false')
+  var unitSizes = document.createElement('ul')
+  $.each(entry.units, function(unit, stats) {
+    var unitChoice = makeUnitStatsEntry(name, unit, stats, false)
+    unitSizes.appendChild(unitChoice)
+  })
+  item.appendChild(unitSizes)
+  $(unitSizes).listview()
+  $(item).collapsible()
+  return item
+}
+
+// TODO resume here
+function makeUnitOptions(options) {
+  var unitOptions = document.createElement('ul')
+  $.each(options, function(label, option) {
+    var optionEntry = document.createElement('li')
+    var anchor = document.createElement('a')
+    anchor.innerHTML = makeUnitStatsTable(name, unit, stats, displayName)
+    $(anchor).attr('href', '#unit-details')
+    $(anchor).on('click', function(event) {
+      army.currentUnit = {
+        name: name,
+        unit: unit,
+        stats: stats
+      }
+    })
+    unitOptions.appendChild(optionEntry)
+  })
+  return unitOptions
+}
+
+// creates a list item that is linked to unit-details page
 function makeUnitStatsEntry(name, unit, stats, displayName) {
   var unitChoice = document.createElement('li')
   var anchor = document.createElement('a')
@@ -49,6 +112,7 @@ function makeUnitStatsEntry(name, unit, stats, displayName) {
   return unitChoice
 }
 
+// creates the tabular layout for the unit entry
 function makeUnitStatsTable(name, unit, stats, displayName) {
   var statsTable = displayName ? '<h3>' + name + '</h3>' : ''
   statsTable += '<table border="0"><tr style="border-bottom: 1px solid grey;">'
