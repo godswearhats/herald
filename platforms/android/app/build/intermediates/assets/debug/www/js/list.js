d971562c0bf23cf9fb1b4f5804f762f124be4f35
Aidan Rogers <arogers@snapchat.com>
2018-03-23 17:12:07 -0700

    Adds files for saving, includes cordova stuff

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/list.js
@@ -0,0 +1,147 @@
+class ArmyList {
+  constructor(label) {
+    this.label = label || 'Unnamed'
+    this.template = null
+    this.entries = []
+    
+    this.hordes = 0
+    this.regiments = 0
+    this.troops = 0
+    this.warEngines = 0
+    this.monsters = 0
+    this.heroes = 0
+    this.points = 0
+  }
+  
+  isValid() {
+    var hard = this.hordes
+    var soft = this.regiments
+    soft = hard + soft - this.warEngines
+    soft = hard + soft - this.monsters
+    soft = hard + soft - this.heroes
+    return (soft >= 0 && this.troops <= (this.hordes * 4 + this.regiments * 2))
+  }
+  
+  // FIXME to include nimble, irregular, individual
+  unitStrength() {
+    return this.hordes * 3 + this.regiments * 2 + this.troops + this.warEngines + this.monsters + this.heroes
+  }
+  
+  drops() {
+    return this.entries.length
+  }
+  
+  addEntry(entry) {
+    this.entries.push(entry)
+
+    if (entry.unit.master.irregular) {
+      this.troops += 1
+    }
+    else {
+      switch (UNIT_SIZES.indexOf(entry.unit.size)) {
+        case SIZE_LEGION:
+        case SIZE_HORDE:
+          this.hordes += 1
+          break
+        case SIZE_REGIMENT:
+          this.regiments += 1
+          break
+        case SIZE_TROOP:
+          this.troops += 1
+          break
+        default:
+          switch (entry.type) {
+            case TYPE_WAR_ENGINE:
+              this.warEngines += 1
+              break
+            case MONSTER:
+              this.monsters += 1
+              break
+            case HERO:
+              this.heroes += 1
+              break
+          }
+      }
+    }
+
+    this.points += entry.points
+  }
+  
+  // static load(label) {
+  //   current.list = new ArmyList(label)
+  //   console.log(current)
+  //   if (localStorage.getItem(label)) {
+  //     let temp = JSON.parse(localStorage.getItem(label))
+  //     temp.forEach(function (data) {
+  //       current.list.addEntry(new ListEntry(current.template, data))
+  //     })
+  //   }
+  //
+  //   console.log('List loading done')
+  //   console.log(current)
+  // }
+}
+
+class ListEntry {
+  constructor() {
+
+  }
+  
+  static entryFromCurrent() {
+    var self = new ListEntry()
+    self.unit = current.unit
+    self.artifact = current.artifact
+    self.spells = current.spells
+    self.options = current.options
+    return self
+  }
+  
+  toHTML() {
+    var self = this
+    let entry = this.unit.toHTML(true, false, function(table) {
+      if (self.artifact) {
+        table += self._addRow(self.artifact)
+      }
+      if (self.spells) {
+        table += self._addRow(self.spells)
+      }
+      if (self.options) {
+        table += self._addRow(self.options)
+      }
+      if (self.artifact || self.spells || self.options) {
+        table += self._addRow({ name: 'Total', points: self.points, style: 'border-top: 1px solid grey' })
+      }
+      return table      
+    })
+    return entry
+    // TODO add artifact, spells, options
+  }
+  
+  get points() {
+    let points = this.unit.points
+    if (this.artifact) { points += this.artifact.points }
+    if (this.spells) { points += this.spells.points }
+    if (this.options) { points += this.options.points }
+    return points
+  }
+  
+  _addRow(item) {
+    let row = document.createElement('tr')
+    let name = document.createElement('td')
+    let points = document.createElement('td')
+    name.setAttribute('colspan', '7')
+    name.setAttribute('style', 'text-align: right')
+    name.innerHTML = item.name
+    points.innerHTML = item.points
+    row.appendChild(name)
+    row.appendChild(points)
+    if (item.style) {
+      points.setAttribute('style', item.style)
+    }
+    return row.outerHTML
+  }
+  
+  toString() {
+    
+  }
+}
---
+++ platforms/ios/www/js/artifact.js
@@ -0,0 +1,59 @@
+class Artifact {
+  constructor(name, data) {
+    this.name = name
+    this.id = data.id
+    this.description = data.description
+    this.rules = data.rules // TODO make it modify specials / spells
+    this.points = data.points
+    this.label = this.name + ' (' + this.points + ')'
+  }
+  
+  asListItem() {
+    let artifact = document.createElement('li')
+    let anchor = document.createElement('a')
+    anchor.innerHTML = this.label
+    $(anchor).attr('href', '#unit-details')
+    var self = this
+    $(anchor).on('click', function(event) {
+      current.artifact = self
+    })
+    artifact.appendChild(anchor)
+    return artifact
+  }
+  
+  toHTML() {
+    let div = document.createElement('div')
+    div.innerHTML = this.label
+    return div
+  }
+  
+  
+}
+
+class ArtifactList {
+  constructor(raw) {
+    const list = []
+    $.each(raw, function(name, data) {
+      list.push(new Artifact(name, data))
+    })
+    this.list = list
+  }
+  
+  artifactWithID(index) {
+    return this.list[index]
+  }
+  
+  asList(element) {
+    if (this.listItems) { 
+      return this.listItems 
+    }
+    let listItems = document.createElement('ul')
+    for (let i = 1; i < this.list.length; i++) { // note index starting at 1
+      listItems.appendChild(this.list[i].asListItem())
+    }
+    listItems.setAttribute('data-filter', 'true')
+    listItems.setAttribute('data-input', '#artifacts-autocomplete')
+    $(listItems).listview()
+    return this.listItems = listItems
+  }
+}
